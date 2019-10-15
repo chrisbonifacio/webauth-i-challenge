ba92258c0be6484493aa99dc76197b935d4dffec
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-10-15 00:11:23 -0400

    create middleware to validate login credentials

---
+++ api/middleware/auth.js
@@ -0,0 +1,17 @@
+const Users = require("../users/userModel")
+
+const bcrypt = require("bcrypt")
+
+async function validateLogin(req, res, next) {
+  const { username, password } = req.body
+
+  const [user] = await Users.findUser(username)
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    next()
+  } else {
+    res.status(400).json({ message: "Invalid Credentials" })
+  }
+}
+
+module.exports = { validateLogin }
